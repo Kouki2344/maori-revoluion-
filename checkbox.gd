ce49caa2f08ee2778275e796021b24d28277dfcb
Kouki2344 <168596541+Kouki2344@users.noreply.github.com>
2025-09-10 19:30:27 +1200

    Mute checkbox

---
+++ checkbox.gd
@@ -0,0 +1,9 @@
+extends CheckBox
+
+func _ready():
+	# Connect signal
+	toggled.connect(_on_mute_toggled)
+
+func _on_mute_toggled(is_muted: bool):
+	var audio_bus_idx = AudioServer.get_bus_index("Master")
+	AudioServer.set_bus_mute(audio_bus_idx, is_muted)
